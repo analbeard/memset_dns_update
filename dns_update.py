b31da715affdc1491a4ab482bf41b319740ce72f
Simon Weald <analbeard@users.noreply.github.com>
2015-01-04 19:32:46 +0000

    added classes

--- dns_update.py
+++ dns_update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Update a single A record in your DNS Manager with your current external IP.
+Update a single A record in your Memset DNS Manager with your current external IP.
 
 Depends on ipgetter (pip install ipgetter)
 """
@@ -10,31 +10,33 @@ from ipgetter import myip
 import logging
 from logging.handlers import SysLogHandler
 
-URI = "https://apikey123:@api.memset.com/v1/xmlrpc/"
-ZONE_RECORD_ID = "abcd1234"
+URI = "https://api_key:@api.provider.com/v1/xmlrpc/"
+ZONE_RECORD_ID = "abcdefg123456"
 
-def config_logging():
-    """ Configures a logger to output to /var/log/syslog""
-    logger = logging.getLogger('dns_update')
-    syslog = SysLogHandler(address='/dev/log')
-    syslog.setFormatter(logging.Formatter('%(filename)s:%(lineno)d: %(levelname)s - %(message)s'))
-    logger.addHandler(syslog)
-    logger.setLevel(logging.INFO)
-    return logger
+class Logger(object):
+    def __init__(self):
+        """ 
+        First we configure a logger to output to /var/log/syslog
+        """
+        logger = logging.getLogger('dns_update')
+        syslog = SysLogHandler(address='/dev/log')
+        syslog.setFormatter(logging.Formatter('%(filename)s:%(lineno)d: %(levelname)s - %(message)s'))
+        logger.addHandler(syslog)
+        logger.setLevel(logging.INFO)
 
-def update_dns():
-    s = ServerProxy(URI)
-    
-    current_dns_ip = s.dns.zone_record_info({"id": ZONE_RECORD_ID})['address']
-    current_ext_ip = myip()
-
-    if current_dns_ip != current_ext_ip:
-        logger.warning("Current DNS record is %s, current IP is %s" % (current_dns_ip, current_ext_ip))
-        logger.info("Setting DNS record to %r" % (current_ext_ip))
-        s.dns.zone_record_update({"id": ZONE_RECORD_ID,"address": current_ext_ip})
-    else:
-        logger.info("Current DNS record is correct, no update needed")
+    def update_dns(self):
+        s = ServerProxy(URI)
+        
+        current_dns_ip = s.dns.zone_record_info({"id": ZONE_RECORD_ID})['address']
+        current_ext_ip = myip()
+        
+        if current_dns_ip != current_ext_ip:
+            logger.warning("Current DNS record is %s, current IP is %s" % (current_dns_ip, current_ext_ip))
+            logger.info("Setting DNS record to %r" % (current_ext_ip))
+            s.dns.zone_record_update({"id": ZONE_RECORD_ID,"address": current_ext_ip})
+        else:
+            logger.info("Current DNS record is correct, no update needed")
 
 if __name__ == "__main__":
-    logger = config_logging()
-    update_dns()
+    logger = Logger()
+    logger.update_dns()
